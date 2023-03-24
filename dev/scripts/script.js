416c3ec0d16fbdb8af1e6827ae34a3bafcbb1905
Kate Khymochka <kkhymochka@my.bcit.ca>
2023-03-24 15:14:32 -0700

    top-bar and hamburger menu

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,19 @@
 // Write your JavaScript here...
+
+// Responsive Menu - Dropdown
+const body = document.body;
+const btnMenu = document.getElementById('btn-menu');
+const nav = document.getElementById('main-navigation');
+
+
+btnMenu.addEventListener('click', openMenu);
+// Prevents the focus state from activating
+btnMenu.addEventListener('mousedown', function (e) {
+  e.preventDefault();
+});
+
+function openMenu() {
+  btnMenu.classList.toggle('clicked');
+  nav.classList.toggle('open');
+
+}
